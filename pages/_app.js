809a03e0f1928718eae9a1e17a9d2d5228b7f649
Justin Friebel <friebj99@gmail.com>
2020-01-16 21:13:28 -0600

    Fix _app.js so we get auto static opt back

--- pages/_app.js
+++ pages/_app.js
@@ -1,60 +1,60 @@
 import React from "react";
-import App, { Container } from "next/app";
+import { Container } from "next/app";
 import Head from "next/head";
 
-export default class MyApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
+function MyApp({ Component, pageProps }) {
+  return (
+    <Container>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto&display=swap"
+          rel="stylesheet"
+          key="google-font-open-sans-roboto"
+        />
+      </Head>
 
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
+      <Component {...pageProps} />
 
-    return { pageProps };
-  }
-
-  render() {
-    const { Component, pageProps } = this.props;
-
-    return (
-      <Container>
-        <Head>
-          <link
-            href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto&display=swap"
-            rel="stylesheet"
-            key="google-font-open-sans-roboto"
-          />
-        </Head>
-
-        <Component {...pageProps} />
-
-        <style global jsx>{`
-          body {
-            font-family: "Roboto", sans-serif;
-            background-color: #26547c;
-            color: #ffd166;
-          }
-          h1,
-          h2,
-          h3,
-          h4,
-          h5,
-          h6 {
-            font-family: "Open Sans", sans-serif;
-            color: #fcfcfc;
-          }
-          ul {
-            padding-inline-start: 0px;
-          }
-          li {
-            list-style-type: none;
-          }
-          a {
-            text-decoration: none;
-            color: #fcfcfc;
-          }
-        `}</style>
-      </Container>
-    );
-  }
+      <style global jsx>{`
+        body {
+          font-family: "Roboto", sans-serif;
+          background-color: #26547c;
+          color: #ffd166;
+        }
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6 {
+          font-family: "Open Sans", sans-serif;
+          color: #fcfcfc;
+        }
+        ul {
+          padding-inline-start: 0px;
+        }
+        li {
+          list-style-type: none;
+        }
+        a {
+          text-decoration: none;
+          color: #fcfcfc;
+        }
+      `}</style>
+    </Container>
+  );
 }
+
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+// MyApp.getInitialProps = async (appContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await App.getInitialProps(appContext);
+//
+//   return { ...appProps }
+// }
+
+export default MyApp;
