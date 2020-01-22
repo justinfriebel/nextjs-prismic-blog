33a1819a6e8eda0ced3b96b5de8e3d2b8f40005f
Justin Friebel <friebj99@gmail.com>
2020-01-21 21:56:54 -0600

    Add some about content

--- pages/about.tsx
+++ pages/about.tsx
@@ -6,7 +6,17 @@ const About: NextPage = () => (
   <Layout>
     <PageHeading heading="About Justin Friebel" />
     <h2>Who am I?</h2>
-    <p>I'm a Software Engineer focused on writing JavaScript.</p>
+    <p>
+      I'm a Software Engineer focused on writing JavaScript. I love playing
+      video games and writing code in my free time. My Boston Terrier Belle
+      loves to play as well!
+    </p>
+
+    <h2>Where am I?</h2>
+    <p>
+      I've lived in cold ass Wisconsin my entire life and would love to
+      eventually live someplace warm someday.
+    </p>
   </Layout>
 );
 
