79c91a838c364569560aadb078d60c26dbf78cab
Justin Friebel <friebj99@gmail.com>
2020-01-28 20:06:10 -0600

    grrr

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,13 @@
-navigator.serviceWorker.getRegistrations().then(function(registrations) {
-  for (let registration of registrations) {
-    registration.unregister();
-  }
-});
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    //returns installed service workers
+    if (registrations.length) {
+      for (let registration of registrations) {
+        registration.unregister();
+      }
+    }
+  });
+}
 
 if ("caches" in window) {
   caches.keys().then(function(keyList) {
