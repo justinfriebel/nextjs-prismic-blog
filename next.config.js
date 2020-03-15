9eadee71a3efca8393d7c68775710e8a9c934f0f
Justin Friebel <friebj99@gmail.com>
2020-03-15 14:53:46 -0500

    try next config with runtime registering

--- next.config.js
+++ next.config.js
@@ -5,7 +5,31 @@ const nextConfig = {
   env: {
     PRISMIC_ACCESS_TOKEN: process.env.PRISMIC_ACCESS_TOKEN
   },
-  dontAutoRegisterSw: true
+  target: "serverless",
+  transformManifest: manifest => ["/"].concat(manifest), // add the homepage to the cache
+  // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
+  // turn on the SW in dev mode so that we can actually test it
+  generateInDevMode: true,
+  workboxOpts: {
+    swDest: "static/service-worker.js",
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: "NetworkFirst",
+        options: {
+          cacheName: "https-calls-v0",
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 60
+          },
+          cacheableResponse: {
+            statuses: [0, 200]
+          }
+        }
+      }
+    ]
+  }
 };
 
 module.exports = withOffline(nextConfig);
