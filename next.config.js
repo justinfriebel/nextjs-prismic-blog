c0d8b3322d0e9e4d6da83dd8e1bd798c49e367ab
Justin Friebel <friebj99@gmail.com>
2020-03-15 22:30:57 -0500

    try dontAutoRegisterSw

--- next.config.js
+++ next.config.js
@@ -10,26 +10,27 @@ const nextConfig = {
   // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
   // turn on the SW in dev mode so that we can actually test it
   generateInDevMode: true,
-  workboxOpts: {
-    swDest: "static/service-worker.js",
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: "NetworkFirst",
-        options: {
-          cacheName: "https-calls-v0",
-          networkTimeoutSeconds: 15,
-          expiration: {
-            maxEntries: 150,
-            maxAgeSeconds: 60
-          },
-          cacheableResponse: {
-            statuses: [0, 200]
-          }
-        }
-      }
-    ]
-  }
+  dontAutoRegisterSw: true
+  // workboxOpts: {
+  //   swDest: "static/service-worker.js",
+  //   runtimeCaching: [
+  //     {
+  //       urlPattern: /^https?.*/,
+  //       handler: "NetworkFirst",
+  //       options: {
+  //         cacheName: "https-calls-v0",
+  //         networkTimeoutSeconds: 15,
+  //         expiration: {
+  //           maxEntries: 150,
+  //           maxAgeSeconds: 60
+  //         },
+  //         cacheableResponse: {
+  //           statuses: [0, 200]
+  //         }
+  //       }
+  //     }
+  //   ]
+  // }
 };
 
 module.exports = withOffline(nextConfig);
