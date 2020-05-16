7be795744c35e30a85dbe26bc4f375c2d57c2717
Justin Friebel <friebj99@gmail.com>
2020-05-16 02:05:20 -0500

    Try next-offline.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,35 @@
 require("dotenv").config();
+const withOffline = require("next-offline");
 
 const nextConfig = {
   env: {
-    PRISMIC_ACCESS_TOKEN: process.env.PRISMIC_ACCESS_TOKEN
-  }
+    PRISMIC_ACCESS_TOKEN: process.env.PRISMIC_ACCESS_TOKEN,
+  },
+  target: "serverless",
+  transformManifest: (manifest) => ["/"].concat(manifest), // add the homepage to the cache
+  // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
+  // turn on the SW in dev mode so that we can actually test it
+  generateInDevMode: true,
+  workboxOpts: {
+    swDest: "static/service-worker.js",
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: "NetworkFirst",
+        options: {
+          cacheName: "https-calls",
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 60,
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+    ],
+  },
 };
 
-module.exports = nextConfig;
+module.exports = withOffline(nextConfig);
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import React from "react";
 import Head from "next/head";
+import { register, unregister } from "next-offline/runtime";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    register();
+    return function cleanup() {
+      unregister();
+    };
+  });
+
   return (
     <React.Fragment>
       <Head>
