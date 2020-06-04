ccc605e3bf76b1f5b2d9b45f21a318f85c1203be
Justin Friebel <friebj99@gmail.com>
2020-06-03 23:14:48 -0500

    Try routing to about to trigger SW.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,39 @@
 import { NextPage } from "next";
 import Layout from "../components/Layout";
 import PageHeading from "../components/PageHeading";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
-const Home: NextPage = () => (
-  <Layout>
-    <PageHeading heading="Hello! You're at Justin Friebel's website." />
-    <p>
-      I mostly write about nerdy software things. I'm currently writing
-      JavaScript for{" "}
-      <a href="https://www.dealerinspire.com/careers/" target="_blank">
-        Dealer Inspire
-      </a>
-      .
-    </p>
-    <p>
-      I also work on my side project,{" "}
-      <a href="https://helpflow.in" target="_blank">
-        helpflow
-      </a>
-      , in my free time.
-    </p>
-  </Layout>
-);
+const Home: NextPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const { pathname } = router;
+    if (pathname == "/") {
+      router.push("/about");
+    }
+  });
+
+  return (
+    <Layout>
+      <PageHeading heading="Hello! You're at Justin Friebel's website." />
+      <p>
+        I mostly write about nerdy software things. I'm currently writing
+        JavaScript for{" "}
+        <a href="https://www.dealerinspire.com/careers/" target="_blank">
+          Dealer Inspire
+        </a>
+        .
+      </p>
+      <p>
+        I also work on my side project,{" "}
+        <a href="https://helpflow.in" target="_blank">
+          helpflow
+        </a>
+        , in my free time.
+      </p>
+    </Layout>
+  );
+};
 
 export default Home;
