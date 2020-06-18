b9326aac159cb4530599ab60559c2907082fbae5
Justin Friebel <friebj99@gmail.com>
2020-06-17 23:56:23 -0500

    Try webfontloader in componentDidMount.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,15 @@
 import Document, { Html, Main, NextScript } from "next/document";
 import WebFont from "webfontloader";
 
-WebFont.load({
-  google: {
-    families: ["Open Sans", "Roboto", "sans-serif"],
-  },
-});
-
 class MyDocument extends Document {
+  componentDidMount() {
+    WebFont.load({
+      google: {
+        families: ["Open Sans", "Roboto", "sans-serif"],
+      },
+    });
+  }
+
   render() {
     return (
       <Html lang="en">
