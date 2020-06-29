3f0f8cf490c83ef8a491b945314e8213249561dc
Justin Friebel <friebj99@gmail.com>
2020-06-28 23:31:26 -0500

    Use NEXT_PUBLIC_ to expose GA ID to browser.

--- gtag.js
+++ gtag.js
@@ -1,4 +1,5 @@
-export const GA_TRACKING_ID = process.env.GOOGLE_ANALYTICS_TRACKING_ID;
+export const GA_TRACKING_ID =
+  process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID;
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,6 +17,7 @@ class MyDocument extends Document {
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
           />
+
           <script
             dangerouslySetInnerHTML={{
               __html: `
