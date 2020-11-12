e5c1d6705308dc0b05a011800c62189731321ca5
Justin Friebel <friebj99@gmail.com>
2020-11-12 08:38:57 +0000

    Update main text color

--- colors.ts
+++ colors.ts
@@ -1,7 +1,7 @@
 export const colors = {
   background: "#083a65",
   mobileNavBackground: "#052b4c",
-  textNormal: "#b3a8ac",
+  textNormal: "#aad8ff",
   link: "#fcfcfc",
   linkHover: "#828282",
   heading: "#d89908",
