fb4e824d94c661eebd3f9119b49f6c3fef438bee
Justin Friebel <friebj99@gmail.com>
2020-11-12 08:35:18 +0000

    Update line height and font size for code slices

--- components/CodeSlice.tsx
+++ components/CodeSlice.tsx
@@ -2,12 +2,20 @@ import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { synthwave84 } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import { RichText } from "prismic-reactjs";
 
+const customStyle = {
+  lineHeight: "1",
+  fontSize: "0.8em",
+};
+
 const CodeSlice = ({ content }) => {
   return (
     <SyntaxHighlighter
       language="javascript"
       style={synthwave84}
       showLineNumbers
+      codeTagProps={{
+        style: customStyle,
+      }}
     >
       {RichText.asText(content)}
     </SyntaxHighlighter>
